2034c5da71b94ca5551dd45b33fa6214852e1b29
Roger Best <rbest@microsoft.com>
2018-09-21 11:00:16 -0700

    Test DSC

---
+++ Samples/DSC/EnableHyperV.ps1
@@ -0,0 +1,16 @@
+Configuration HyperVScriptResource
+{
+	Node localhost
+	{
+		WindowsFeature IIS
+		{
+			Ensure="Present"
+			Name="Web-Server"
+		}
+		WindowsFeature ASP
+		{
+			Ensure="Present"
+			Name="Web-Asp-Net45"
+		}
+	}
+}
